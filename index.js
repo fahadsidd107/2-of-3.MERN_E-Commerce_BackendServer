163a52c92dc0fbceeb5043fc397f770d3d8bc4d6
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-14 01:28:10 +0500

    work complete

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const mongoose = require('mongoose');
 const PORT = 7777
 
-mongoose.connect('mongodb+srv://admin:admin@cluster0.crcc2.mongodb.net/ecommerce?retryWrites=true&w=majority');
+// mongoose.connect('mongodb+srv://admin:admin@cluster0.crcc2.mongodb.net/ecommerce?retryWrites=true&w=majority');
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);;
 })
