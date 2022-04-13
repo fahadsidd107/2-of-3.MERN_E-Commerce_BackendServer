e29ea04db03233735ac6a6b48b0adc8aeddecda2
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-14 01:03:23 +0500

    work complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
+const PORT = 7777
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-}
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);;
+})
