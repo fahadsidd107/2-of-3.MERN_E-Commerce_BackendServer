5d91499397a9ecc97be30f43ebe385028df25843
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-15 00:53:59 +0500

    work complete

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const PORT = 7777
+const dotenv = require('dotenv');
+dotenv.config();
 
-mongoose.connect('mongodb+srv://admin:admin@cluster0.crcc2.mongodb.net/shop?retryWrites=true&w=majority'
-).then(()=>console.log('Database Connected')).catch((err)=>console.log(err))
+mongoose.connect( process.env.MANGO_DB_URL)
+.then(()=>console.log('Database Connected'))
+.catch((err)=>console.log(err))
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);;
