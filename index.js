346521462e7f091507da55c843c3bafaf8a01495
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-15 00:46:34 +0500

    work complete

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const PORT = 7777
 
 mongoose.connect('mongodb+srv://admin:admin@cluster0.crcc2.mongodb.net/shop?retryWrites=true&w=majority'
-).then(()=>console.log('Data')).catch()
+).then(()=>console.log('Database Connected')).catch((err)=>console.log(err))
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);;
