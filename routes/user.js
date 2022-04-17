9156a5d5c53fbc9cea8e2166dc4d1f785904d998
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-17 19:56:47 +0500

    work complete

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 
-router.get('/usergettest', (req, res) => {
-  res.send('Test is succesfull');
-}  );   // end of router.get 
+// router.get('/usergettest', (req, res) => {
+//   res.send('Test is succesfull');
+// }  );   // end of router.get 
 
-router.post('/userposttest', (req, res) => {
-  const uname = req.body.uname;
-  console.log(uname);
-  res.send(`Test is succesfull ${uname}`);
-}  );   // end of router.post 
+// router.post('/userposttest', (req, res) => {
+//   const uname = req.body.uname;
+//   console.log(uname);
+//   res.send(`Test is succesfull ${uname}`);
+// }  );   // end of router.post 
 
 module.exports = router;
