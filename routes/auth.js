1d3f239df99ea5cd4e8cda81a3efd8bf67dc50a9
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-18 21:28:50 +0500

    work complete

--- routes/auth.js
+++ routes/auth.js
@@ -2,16 +2,17 @@ const router = require('express').Router();
 const User = require('./../models/user');
 
 //Register
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
         password: req.body.password,
     })
     try{
-        const savedUser = newUser.save();
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
     }catch(err){
-        console.log(err);
+       
     }
 })
 
