5a88aa35a883e17c355d3b1958b8b0b67bd7747a
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-18 21:17:01 +0500

    work complete

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,12 @@
 const router = require('express').Router();
+const User = require('./../models/user');
+
+//Register
+router.post('/register', (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+    })
+})
 
 
 module.exports = router;
