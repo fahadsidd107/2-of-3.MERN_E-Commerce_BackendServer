f7b342ed27c101c43d5903ced4791af599a221df
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-19 21:38:51 +0500

    work complete

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('./../models/user');
+const CryptoJS = require('crypto-js')
 
 //Register
 router.post('/register', async (req, res) => {
