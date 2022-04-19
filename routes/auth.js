6c540d154b21293a419c3fc91a6324bf4b95460d
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-19 21:54:53 +0500

    work complete

--- routes/auth.js
+++ routes/auth.js
@@ -20,4 +20,14 @@ router.post('/register', async (req, res) => {
 })
 
 
+//Login
+router.post('/login',async (req,res)=>{
+    try{
+
+        
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
