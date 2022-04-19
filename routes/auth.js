7c583c8e0cdce157abb90340724d049b543585e0
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-19 22:00:19 +0500

    work complete

--- routes/auth.js
+++ routes/auth.js
@@ -23,8 +23,8 @@ router.post('/register', async (req, res) => {
 //Login
 router.post('/login',async (req,res)=>{
     try{
+ const user = await User.findOne({email: req.body.email})
 
-        
     }catch(err){
         res.status(500).json(err);
     }
