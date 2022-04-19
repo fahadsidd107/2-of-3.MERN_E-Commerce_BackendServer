8a873a94c72b61241ec66c630ec30948be67f5cf
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-19 21:44:22 +0500

    work complete

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 const User = require('./../models/user');
-const CryptoJS = require('crypto-js')
+const CryptoJS = require('crypto-js');
+const dotenv = require('dotenv');
+dotenv.config();
 
 //Register
 router.post('/register', async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: CryptoJS.AES.encrypt(req.body.password, "Secret Passphrase"),
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
     })
     try{
         const savedUser = await newUser.save();
