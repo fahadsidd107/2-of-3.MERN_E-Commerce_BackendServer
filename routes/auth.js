aa882eb22cda4c31e41b465c140ec3b151a64d7f
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-19 21:40:05 +0500

    work complete

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ router.post('/register', async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt("Message", "Secret Passphrase"),
     })
     try{
         const savedUser = await newUser.save();
