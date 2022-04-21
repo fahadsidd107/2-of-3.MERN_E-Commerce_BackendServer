805b38503fdae0c5fc19a41904f8d2719fd0b99e
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-21 21:48:52 +0500

    work complete

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
     const authHeader = req.headers.token;
     if (authHeader){
-
+jwt.verify(token , )
     }else{
  return res.status(401).send({error: 'No token provided'});
     }
