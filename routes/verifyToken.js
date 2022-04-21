c1da1af326e1d5991b13f72b41fc787f8ec933a0
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-21 21:51:02 +0500

    work complete

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,9 +1,17 @@
 const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+dotenv.config();
 
 const verifyToken = (req, res, next) => {
     const authHeader = req.headers.token;
     if (authHeader){
-jwt.verify(token , )
+jwt.verify(token , rocess.env.JWT_SEC , (err,user)=>{
+    if (err){
+        return res.status(401).send({err: 'No token provided'});      
+    }else{
+        
+    }
+})
     }else{
  return res.status(401).send({error: 'No token provided'});
     }
