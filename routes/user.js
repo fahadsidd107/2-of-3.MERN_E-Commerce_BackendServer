c4bd9e79be29ae7afa0b36e151cca12fba654649
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-23 01:14:44 +0500

    work complete

--- routes/user.js
+++ routes/user.js
@@ -48,12 +48,12 @@ router.delete("/:id", verifyTokenAndAuthorization , async (req,res)=>{
 })
 
 //GET USER
-router.get("/:id", verifyTokenAndAdmin , async (req,res)=>{
+router.get("/find/:id", verifyTokenAndAdmin , async (req,res)=>{
   try{
     const user= await User.findById(req.params.id);
     const {password , ...others} = user._doc;
 
-    res.status(200).json({...others , accessToken});
+    res.status(200).json(others);
   }catch(err){
     res.status(500).json(err);
   }
