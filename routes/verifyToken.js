e339e74795764924ba73f4f6d556a337ee05ba42
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-22 23:23:34 +0500

    work complete

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -27,4 +27,15 @@ const verifyTokenAndAuthorization = (req,res,next)=>{
     })
 }
 
+
+const verifyTokenAndAdmin = (req,res,next)=>{
+    verifyToken(req,res,()=>{
+        if(req.user.id === req.params.id || req.user.isAdmin){
+            next()
+        }else{
+            res.status(403).send({error:"You are not authorized todo this action"})
+        }
+    })
+}
+
 module.exports={ verifyToken,verifyTokenAndAuthorization };
