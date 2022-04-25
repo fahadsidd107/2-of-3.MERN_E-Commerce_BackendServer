b0ac7065e3a121b97b23c341697f49a674b7dcb1
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-25 20:55:40 +0500

    work complete

--- routes/product.js
+++ routes/product.js
@@ -22,7 +22,8 @@ router.post("/",verifyTokenAndAdmin,async(req,res)=>{
   const newProduct = new Product(req.body);
 
   try{
-
+const savedProduct = await newProduct.save();
+res.status(200).send(savedProduct);
   }catch(err){
     res.status(500).send(err);
   }
