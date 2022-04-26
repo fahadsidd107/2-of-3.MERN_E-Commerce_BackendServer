b2f027a0352cfa7c948e3399d5efc1420450d9b1
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-26 19:41:38 +0500

    work complete

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -3,10 +3,10 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
   if (authHeader) {
-      const token = authHeader.split(" ")[1];
+    const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.JWT_SEC, (err, user) => {
       if (err) {
-        return res.status(403).send("token is not valid" );
+        return res.status(403).send("token is not valid");
       } else {
         req.user = user;
         next();
@@ -17,25 +17,32 @@ const verifyToken = (req, res, next) => {
   }
 };
 
-const verifyTokenAndAuthorization = (req,res,next)=>{
-    verifyToken(req,res,()=>{
-        if(req.user.id === req.params.id || req.user.isAdmin){
-            next()
-        }else{
-            res.status(403).send({error:"You are not authorized todo this action"})
-        }
-    })
-}
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res
+        .status(403)
+        .send({ error: "You are not authorized todo this action" });
+    }
+  });
+};
 
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res
+        .status(403)
+        .send({ error: "You are not authorized todo this action" });
+    }
+  });
+};
 
-const verifyTokenAndAdmin = (req,res,next)=>{
-    verifyToken(req,res,()=>{
-        if(req.user.isAdmin){
-            next()
-        }else{
-            res.status(403).send({error:"You are not authorized todo this action"})
-        }
-    })
-}
-
-module.exports={ verifyToken,verifyTokenAndAuthorization,verifyTokenAndAdmin };
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
