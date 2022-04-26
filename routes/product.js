13e0dde210d3fd101eeb4d9ab8dbae79eb9c071d
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-26 19:36:53 +0500

    work complete

--- routes/product.js
+++ routes/product.js
@@ -57,12 +57,10 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
 });
 
 //GET PRODUCT
-router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+router.get("/find/:id", async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
-    const { password, ...others } = user._doc;
-
-    res.status(200).json(others);
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
   } catch (err) {
     res.status(500).json(err);
   }
