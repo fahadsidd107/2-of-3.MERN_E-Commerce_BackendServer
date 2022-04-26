b2a1f4ea9bfd2f1bedc4fe926db242ea0359e414
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-26 19:38:59 +0500

    work complete

--- models/Cart.js
+++ models/Cart.js
@@ -4,17 +4,16 @@ const CartSchema = new mongoose.Schema(
   {
     userID: { type: String, required: true },
     products: [
-        {
-            productId: {
-              type: String,
-            },
-            quantity: {
-              type: Number,
-              default: 1,
-            },
-          },
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
     ],
-
   },
   { timestamps: true }
 );
