becd027a1810731f2ef7f93752db77357ce630b0
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-27 00:17:35 +0500

    work complete

--- routes/product.js
+++ routes/product.js
@@ -55,7 +55,7 @@ router.get("/find/:id", async (req, res) => {
 });
 
 //GET ALL PRODUCTS
-router.get("/", verifyTokenAndAdmin, async (req, res) => {
+router.get("/", async (req, res) => {
   const query = req.query.new;
   try {
     const users = query
