3658a0ce9ba39574ac1f9d23dddf64cff6ac2e3b
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-26 19:38:17 +0500

    work complete

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const PORT = 7777
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const PORT = 7777;
+const dotenv = require("dotenv");
 dotenv.config();
 app.use(express.json());
-const userRoute = require('./routes/user');
-const authRoute = require('./routes/auth');
-const productRoute = require('./routes/product');
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 
-mongoose.connect( process.env.MANGO_DB_URL)
-.then(()=>console.log('Database Connected'))
-.catch((err)=>console.log(err))
+mongoose
+  .connect(process.env.MANGO_DB_URL)
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log(err));
 
 // app.get('/api' , ()=>console.log('Test API'))
 
-app.use('/api/auth', authRoute);
-app.use('/api/user', userRoute);
-app.use('/api/product', productRoute);
-
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/product", productRoute);
 
 app.listen(process.env.PORT || PORT, () => {
-  console.log(`Server is running on port ${PORT}`);;
-})
+  console.log(`Server is running on port ${PORT}`);
+});
