2936e0d124ab98471790c5055796163e05d4b054
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-26 19:37:22 +0500

    work complete

--- routes/product.js
+++ routes/product.js
@@ -6,16 +6,6 @@ const {
   verifyTokenAndAdmin,
 } = require("./verifyToken");
 
-// router.get('/usergettest', (req, res) => {
-//   res.send('Test is succesfull');
-// }  );   // end of router.get
-
-// router.post('/userposttest', (req, res) => {
-//   const uname = req.body.uname;
-//   console.log(uname);
-//   res.send(`Test is succesfull ${uname}`);
-// }  );   // end of router.post
-
 
 //CREATE PRODUCT 
 router.post("/",verifyTokenAndAdmin,async(req,res)=>{
