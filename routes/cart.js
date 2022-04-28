8ac7c17e47e54a0aa873764f859b132d9cbadbbe
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-29 00:42:34 +0500

    work complete

--- routes/cart.js
+++ routes/cart.js
@@ -34,15 +34,15 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
-// //DELETE PRODUCT
-// router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
-//   try {
-//     await Product.findByIdAndDelete(req.params.id);
-//     res.status(200).json("Product deleted");
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+//DELETE PRODUCT
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json("Product deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // //GET PRODUCT
 // router.get("/find/:id", async (req, res) => {
