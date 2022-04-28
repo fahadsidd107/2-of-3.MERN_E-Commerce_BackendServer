d6260d118fbd37f14a7d18ff4f05474faa59edb8
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-29 00:56:22 +0500

    work complete

--- routes/cart.js
+++ routes/cart.js
@@ -47,32 +47,15 @@ router.delete("/:id", verifyTokenAndAuthorization , async (req, res) => {
 //GET USER CART
 router.get("/find/:id", verifyTokenAndAuthorization , async (req, res) => {
   try {
-    const cart = await Cart.findById(req.params.id);
+    const cart = await Cart.findOne(req.params.id);
     res.status(200).json(cart);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// //GET ALL PRODUCTS
-// router.get("/", async (req, res) => {
-//   const Newquery = req.query.new;
-//   const Categoryquery = req.query.category;
-//   try {
-// let products
-
-//     if (Newquery) {
-//       products = await Product.find().sort({ createdAt: -1 }).limit(1);
-//     }else if(Categoryquery){
-//       products = await Product.find({categories:{$in:[Categoryquery]}});
-//     }else{
-//       products = await Product.find();
-//     }
-//     res.status(200).json(products);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+//GET ALL 
+router.get()
 
 
 
