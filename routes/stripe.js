0a7d79e5c7f8250892cb4de9445a3a108074ad28
fahadsidd107 <fsiddiqui107@gmail.com>
2022-05-03 19:33:19 +0500

    work complete

--- routes/stripe.js
+++ routes/stripe.js
@@ -2,7 +2,10 @@ const router = require("express").Router();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 router.post("/payment", (req, res) => {
-    stripe.charges.create({})
+    stripe.charges.create({
+        source: req.body.tokenId,
+        amount: req.body.amount,
+    })
 })
 
 module.exports = router;
