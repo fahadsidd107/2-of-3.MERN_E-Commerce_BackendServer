fed2e3c36955da4c050a6e6667082f1441878320
fahadsidd107 <fsiddiqui107@gmail.com>
2022-07-02 13:22:08 +0500

    work complete

--- routes/product.js
+++ routes/product.js
@@ -18,7 +18,7 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
-//UPDATE ONE PRODUCT
+//UPDATE PRODUCT
 router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
   try {
     const updatedProduct = await Product.findByIdAndUpdate(
